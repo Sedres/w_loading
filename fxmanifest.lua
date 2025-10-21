53adc6c0dec873ebf7e113ff33d7ab6abefc96f1
Sedres <alincalauz19@gmail.com>
2025-10-21 10:10:33 +0200

    Fixed bad syntax in fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -12,17 +12,17 @@ client_scripts {
 }
 
 -- ui_page 'ui/index.html'
---[[ files {
+files {
 	'ui/index.html',
 	'ui/assets/*.*',
 	'config/Music/*.*',
 	'config/Background/*.*',
 	'config/*.json',
-} ]]
+}
 
 loadscreen 'ui/index.html'
 loadscreen_cursor 'yes'
 loadscreen_manual_shutdown 'yes' -- Comment to disable manual shutdown
 
 
-ui_page 'ui/index.html'
+--[[ ui_page 'ui/index.html' ]]
